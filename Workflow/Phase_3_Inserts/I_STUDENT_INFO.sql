0b2006a0a36ed9f8437655fd858b2bc64f0e14ab
Anthony <AntC.Randall@gmail.com>
2024-12-01 21:52:10 -0500

    Add files via upload

---
+++ Workflow/Phase_3_Inserts/I_STUDENT_INFO.sql
@@ -0,0 +1,10 @@
+INSERT INTO STUDENT_INFO VALUES ('Archie', 'Niblett', 'Male', '6316507843', 11772, 33602, '6 Lee Ave', 'Patchogue', 'NY', 02774);
+INSERT INTO STUDENT_INFO VALUES ('Lester', 'Brown', 'Male', '6314568241', 11967, 33811, '167 Northern Boulevard', 'Shirley', 'NY',  88473);
+INSERT INTO STUDENT_INFO VALUES ('Benjamin', 'Guy', 'Other', '6313988871', 11705, 33146, '411 Middle Road', 'Bayport', 'NY', 77561);
+INSERT INTO STUDENT_INFO VALUES ('Ada', 'Winter', 'Female', '6317649846', 11953, 33608, '2 Avon Lane', 'Middle Is.', 'NY', 77491);
+INSERT INTO STUDENT_INFO VALUES ('Mae', 'Smith', 'Female', '6314567342', 11967, 33811, 'Lot 39 Flintlock Drive', 'Shirley', 'NY', 88163);
+INSERT INTO STUDENT_INFO VALUES ('Harry', 'Dore', 'Male', '5168763998', 11787, 33538, '74 Cornell Drive', 'Smithtown', 'NY', 10740);
+INSERT INTO STUDENT_INFO VALUES ('Curtis', 'Brown', 'Male', '6316587793', 11730, 33516, '4 Amber Court', 'East Islip', 'NY', 88641);
+INSERT INTO STUDENT_INFO VALUES ('Esther', 'King', 'Other', '5163564886', 11967, 33811, '78 Palmetto Drive', 'Shirley', 'NY', 57361);
+INSERT INTO STUDENT_INFO VALUES ('Allyn', 'Hoggard', 'Male', '6319294490', 11754, 33538, '125 W Main Street', 'Kings Park', 'NY', 99631);
+INSERT INTO STUDENT_INFO VALUES ('Peter', 'Thompson', 'Male', '5164786635', 11705, 33146, '260 S Gillette Avenue', 'Bayport', 'NY', 88571);
